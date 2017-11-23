ea0919670cab393bd9b93f6cc4d7bf054c6f17ea
pkirklewski <kirklewskis@gmail.com>
2017-11-23 17:47:53 +0000

    first commit

---
+++ ASPFileUpload/FileUpload.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FileUpload.aspx.cs" Inherits="ASPFileUpload.FileUpload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>FileUpload</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+<%--    <p><input type="file" id="File1"/></p>
+        <p><input id="Submit1" type="submit" value="submit" /></p>--%>
+        
+        	<p><input type="file" id="File1" name="File1" runat="server" /></p>
+        	<p><input type="submit" id="Submit1" value="Upload" runat="server"/></p>
+       
+    </form>       
+</body>
+</html>
---
+++ ASPFileUpload/FileUpload.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPFileUpload
+{
+    public partial class FileUpload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Page Load OK");
+        }
+
+        private void Submit1_ServerClick(object sender, System.EventArgs e)
+        {
+            Response.Write("click");
+            
+
+            
+
+            if ((File1.PostedFile != null) && (File1.PostedFile.ContentLength > 0))
+            {
+                string fn = System.IO.Path.GetFileName(File1.PostedFile.FileName);
+                string SaveLocation = Server.MapPath("Data") + "\\" + fn;
+                try
+                {
+                    File1.PostedFile.SaveAs(SaveLocation);
+                    Response.Write("The file has been uploaded.");
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("Error: " + ex.Message);
+                }
+            }
+            else
+            {
+                Response.Write("Please select a file to upload.");
+            }
+        }
+    }
+}
